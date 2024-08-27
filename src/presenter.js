ebaafe77bc35bce1d31b4f3ab0737a07f15b4553
Alejandra Palza Morales <alejandra.palza.morales@gmail.com>
2024-08-26 22:54:09 -0400

    Saludar por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,10 @@
 const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
+const nombre=document.querySelector("#nombre");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const saludo='hola';
-  div.innerHTML = "<p>" + saludo + "</p>";
+  const nombreS=String(nombre.value);
+  const saludo='Hola';
+  div.innerHTML = "<p>"+saludo +"<b> " + nombreS + "</b></p>";
 });
