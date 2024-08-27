8be374506dd4462643ace79360548f0a7b586820
Alejandra Palza Morales <alejandra.palza.morales@gmail.com>
2024-08-26 22:59:03 -0400

    Saludar por genero

--- src/presenter.js
+++ src/presenter.js
@@ -5,6 +5,28 @@ const nombre=document.querySelector("#nombre");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const nombreS=String(nombre.value);
-  const saludo='Hola';
-  div.innerHTML = "<p>"+saludo +"<b> " + nombreS + "</b></p>";
+  obtenerSaludoPorGenero(nombreS);
+
 });
+
+// Función para obtener el saludo por género
+async function obtenerSaludoPorGenero(nombre) {
+  try {
+      const response = await fetch(`https://api.genderize.io?name=${nombre}`);
+      const data = await response.json();
+
+      if (data.gender) {
+        let saludo;
+        if (data.gender === 'female') {
+            saludo = 'Sra.';
+        } else {
+            saludo = 'Sr.';
+        }
+          div.innerHTML = "<p> Hola "+saludo +"<b> " + nombre + "</b></p>";
+      } else {
+          console.log(`No se pudo determinar el género para ${nombre}`);
+      }
+  } catch (error) {
+      console.error('Error al obtener información del género:', error);
+  }
+}
